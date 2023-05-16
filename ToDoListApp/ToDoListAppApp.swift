ce442ceab0c09d07e727b20a1de7bb232c6e4679
Nimrat Khalsa <nimratkhalsa16@icloud.com>
2023-05-16 20:31:46 +0530

    Initial Commit

---
+++ ToDoListApp/ToDoListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListAppApp.swift
+//  ToDoListApp
+//
+//  Created by Nimrat Khalsa on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
